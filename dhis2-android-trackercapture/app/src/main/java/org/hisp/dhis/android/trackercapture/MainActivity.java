8273c278682a36326b5c254bb0ef5c944f670794
DESKTOP-E26LDSC\siva0 <sivaadde@iupui.edu>
2017-04-24 12:02:02 -0400

    Hamburger Icon added(Tracker App)

--- dhis2-android-trackercapture/app/src/main/java/org/hisp/dhis/android/trackercapture/MainActivity.java
+++ dhis2-android-trackercapture/app/src/main/java/org/hisp/dhis/android/trackercapture/MainActivity.java
@@ -30,8 +30,11 @@
 package org.hisp.dhis.android.trackercapture;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -48,8 +51,12 @@ import org.hisp.dhis.android.trackercapture.fragments.selectprogram.SelectProgra
 
 
 public class MainActivity extends AppCompatActivity implements INavigationHandler {
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
     public final static String TAG = MainActivity.class.getSimpleName();
     private OnBackPressedListener mBackPressedListener;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,11 +73,23 @@ public class MainActivity extends AppCompatActivity implements INavigationHandle
         LoadingController.enableLoading(this, ResourceType.RELATIONSHIPTYPES);
         Dhis2Application.bus.register(this);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar, R.string.drawer_open,
+                R.string.drawer_close);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
 
         PeriodicSynchronizerController.activatePeriodicSynchronizer(this);
         showSelectProgramFragment();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
     }
 
     public void loadInitialData() {
