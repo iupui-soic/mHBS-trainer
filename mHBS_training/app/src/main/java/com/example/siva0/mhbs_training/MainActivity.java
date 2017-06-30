b05bc4bdb95366fbc0ab2ea3495b7c809d1c8eca
DESKTOP-E26LDSC\siva0 <sivaadde@iupui.edu>
2017-06-30 15:13:58 -0400

    Both the applications compile sucessfully.

---
+++ mHBS_training/app/src/main/java/com/example/siva0/mhbs_training/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.siva0.mhbs_training;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
