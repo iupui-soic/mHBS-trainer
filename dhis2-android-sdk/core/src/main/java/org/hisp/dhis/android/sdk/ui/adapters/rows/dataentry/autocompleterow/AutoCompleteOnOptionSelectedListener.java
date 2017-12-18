6738caffaf71add23ce600abbdcaec256ab547d1
Liz <ElisabethZ.Meyers@gmail.com>
2017-12-18 16:50:11 -0500

    updated with new tracker-capture SDK as a submodule
    removed extra folder

--- dhis2-android-sdk/core/src/main/java/org/hisp/dhis/android/sdk/ui/adapters/rows/dataentry/autocompleterow/AutoCompleteOnOptionSelectedListener.java
+++
@@ -1,8 +0,0 @@
-package org.hisp.dhis.android.sdk.ui.adapters.rows.dataentry.autocompleterow;
-
-import org.hisp.dhis.android.sdk.persistence.models.BaseValue;
-import org.hisp.dhis.android.sdk.ui.dialogs.AutoCompleteDialogFragment;
-
-public interface AutoCompleteOnOptionSelectedListener extends AutoCompleteDialogFragment.OnOptionSelectedListener {
-    void setValue(BaseValue value);
-}
