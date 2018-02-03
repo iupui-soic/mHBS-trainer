5632298a1601ab5eaaa995ccbe9fee1f56555e7a
Liz Meyers <ElisabethZ.Meyers@gmail.com>
2018-02-03 14:18:38 -0500

    Allow automatic login from Tracker-Capture
    Added sharedLoginActivity and Broadcast sender to receive login Data from tracker-capture
    
    Update README.md, changed build tools to match tracker-capture,
    Updated application to new version, added ignore to gitmodules to ignore individual updating, applied hotfix for Starting Application from tracker exception,

--- mHBS_training/app/src/main/java/edu/iupui/soic/biohealth/plhi/mhbs/mhbsTrainingApplication.java
+++ mHBS_training/app/src/main/java/edu/iupui/soic/biohealth/plhi/mhbs/mhbsTrainingApplication.java
@@ -3,38 +3,35 @@ package edu.iupui.soic.biohealth.plhi.mhbs;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
+
 import io.fabric.sdk.android.Fabric;
 
 import org.hisp.dhis.android.sdk.controllers.DhisController;
-import org.hisp.dhis.android.sdk.controllers.PeriodicSynchronizerController;
 import org.hisp.dhis.android.sdk.controllers.metadata.MetaDataController;
-import org.hisp.dhis.android.sdk.network.Session;
 import org.hisp.dhis.android.sdk.persistence.Dhis2Application;
 import org.hisp.dhis.android.sdk.persistence.models.UserAccount;
-import org.hisp.dhis.android.sdk.persistence.preferences.AppPreferences;
-import org.hisp.dhis.android.sdk.persistence.preferences.LastUpdatedManager;
-import org.hisp.dhis.android.sdk.ui.activities.LoginActivity;
-
-import java.io.FileInputStream;
-import java.security.KeyStore;
 
 public class mhbsTrainingApplication extends Dhis2Application {
+    static final boolean isLogged = true;
 
     @Override
     public void onCreate() {
-        super.onCreate();
 
-        /*TODO: Here we can send a broadcast to tracker capture. If the user is logged in,        *
+        /*TODO: Here we can send a broadcast to tracker capture. If the user is logged in,
+        * Create an instance of LoginActivity and call handleUser();
+        *
         * Consider adding a prompt to confirm the user is logged in for
         * the case a user is logged in tracker capture while a separate user
         * wants to user our app
         *
         * Consider adding logout functionality
          */
+        super.onCreate();
         final Fabric fabric = new Fabric.Builder(this)
                 .kits(new Crashlytics())
                 .debuggable(true)
@@ -42,18 +39,35 @@ public class mhbsTrainingApplication extends Dhis2Application {
         Fabric.with(fabric);
 
 
+        // if we are logged in, just log in
         UserAccount userAccount = MetaDataController.getUserAccount();
-        if(userAccount!=null) {
+        if (userAccount != null) {
             Class<?> mainActivity = getMainActivity();
             Intent i = new Intent(getApplicationContext(), mainActivity);
-            Log.d("Test", "in mhbs Training Application");
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(i);
+        } else if (isTrackerInstalled(getString(R.string.trackerCapture))) {
+            // broadcast to tracker to receive login information
+            Intent intent = new Intent();
+            intent.setAction("edu.iupui.soic.biohealth.plhi.mhbs.activities.SharedLoginActivity");
+            sendBroadcast(intent);
         }
-
     }
 
     @Override
     public Class<? extends AppCompatActivity> getMainActivity() {
         return new MainActivity().getClass();
+    }
+
+    public boolean isTrackerInstalled(String checkForPackage) {
+        PackageManager pm = this.getPackageManager();
+        boolean installed;
+        try {
+            pm.getPackageInfo(checkForPackage, PackageManager.GET_ACTIVITIES);
+            installed = true;
+        } catch (PackageManager.NameNotFoundException e) {
+            installed = false;
+        }
+        return installed;
     }
 }
