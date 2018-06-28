8c946b3fb836315df0829d31139df259dcacfd30
Liz Meyers <ElisabethZ.Meyers@gmail.com>
2018-06-28 18:18:31 -0400

    hotfix for code merge, fixed plugin issues
    remove unecessary folder

--- www/js/app.js
+++ www/js/app.js
@@ -82,6 +82,13 @@ var catalogView = app.views.create('#view-catalog', {
 var settingsView = app.views.create('#view-settings', {
   url: '/settings/'
 });
+var videoView = app.views.create('#view-video', {
+  url: '/mhbsvideos/'
+});
+var guideView = app.views.create('#view-guide', {
+  url: '/mhbsmain/'
+});
+
 
 var videoListView = app.views.create('#view-videoList', {
   url: '/videoList/'
@@ -514,7 +521,6 @@ var ss = function () {
   return new cordova.plugins.SecureStorage(
     function () {
       console.log("Storage Created");
-      app.data.secureStorage = true;
     },
     securityFunction,
     'mHBS_Hybridapp');
@@ -646,4 +652,165 @@ function getCredentials() {
 function parseCredentials(intent) {
   return intent.extras['key:loginRequest'];
 }
+
+
+var myPhotoBrowserStandalone;
+var myPhotoBrowserPopupDark;
+
+$$(document).on('page:init', '.page[data-name="testvideo"]', function (e) {
+  	console.log("testvideo page loaded");
+myPhotoBrowserStandalone = app.photoBrowser.create({
+  photos: [
+  "file:///android_asset/www/img/image1.jpg",
+  "file:///android_asset/www/img/image2.jpg",
+  ],
+  theme: 'dark'
+});
+console.log("photo loaded");
+myPhotoBrowserPopupDark = app.photoBrowser.create({
+    photos : [
+        {
+            html: '<iframe src="file:///android_asset/www/img/vid/test.mp4" frameborder="0" allowfullscreen></iframe>',
+            caption: 'mHBS sample video (Official HD Video)'
+        },
+    ],
+    theme: 'dark',
+    type: 'standalone',
+	navbar: true,
+	toolbar: false,
+});
+console.log("video loaded");
+});
+
+$$(document).on('click',".pb-standalone-video", function () {
+	console.log("in video photoBrowser function");
+    myPhotoBrowserPopupDark.open();
+});
+
+$$(document).on('click',".pb-videoplayer1", function () {
+	console.log("in videoplayer1");
+	videoView.router.navigate('/mediaplayer/');
+	console.log("page navigated");
+
+
+});
+$$(document).on('page:init', '.page[data-name="mediaplayer"]', function (e) {
+
+	console.log("media player init");
+$$(".videoAdd").css(
+{
+	'position':'absolute',
+    'right': '0',
+    'bottom': '0',
+    'min-width': '100%',
+    'min-height': '100%',
+    'width': '100%',
+    'height': 'auto',
+    'z-index': '-100',
+    'background-size': 'cover',
+    'overflow': 'hidden'
+}
+);
+
+$$("#myBtn").css(
+{
+    'font-size': '17px'
+}
+);
+
+$$("#progress-bar").css(
+{
+	//'left':'16.5%',
+	'top':'84%',
+    'font-size': '6px',
+	'padding':'6px'
+}
+);
+$$("#seek").css(
+{
+	//'left':'74.3%',
+    'font-size': '16px',
+	'width':'90%'
+}
+);
+$$("#seekButton1").css(
+{
+    'font-size': '35px'
+}
+);
+$$("#seekButton2").css(
+{
+    'font-size': '35px'
+}
+);
+
+$$("#bar1").css(
+{
+	'position':'fixed',
+    'top':'70%',
+	'width':'100%'
+}
+);
+
+
+
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+
+$$('#myBtn').on('click', function () {
+	console.log("In function");
+     	if (video.paused) {
+			video.play();
+			btn.innerHTML = "Pause";
+			btn.className="col button button-small button-round button-fill color-red";
+		} else {
+			video.pause();
+			btn.innerHTML = "Play";
+			btn.className="col button button-small button-round button-fill color-green";
+		}
+    });
+
+$$('#vol-filter').on('range:change', function (e, range) {
+	video.volume=range.value;
+});
+
+
+$$('#seekButton2').on('click', function (e) {
+	console.log("+");
+	video.currentTime=video.currentTime+2.5;
+});
+$$('#seekButton1').on('click', function (e) {
+	console.log("-");
+	video.currentTime=video.currentTime-2.5;
+});
+
+$$('#myVideo').on('timeupdate', function (e) {
+	var pos=video.currentTime * (100/video.duration);
+	app.progressbar.set('#progress-bar',pos);
+});
+
+$$('#myVideo').on('timeupdate', function (e) {
+	var curmin = Math.floor(video.currentTime/60);
+	var cursec = Math.floor(video.currentTime - curmin * 60);
+	var durmin = Math.floor(video.duration/60);
+	var dursec = Math.floor(video.duration - durmin * 60);
+	var cutime = document.getElementById("currenttime");
+	var dutime = document.getElementById("durationtime");
+	if(dursec <10){
+		dursec = "0"+dursec;
+	}
+	if(durmin <10){
+		durmin = "0"+durmin;
+	}
+
+	if(cursec < 10){
+		cursec = "0"+cursec;
+	}
+	if(curmin < 10){
+		curmin = "0"+curmin;
+	}
+	cutime.innerHTML = curmin + ":"+cursec;
+	dutime.innerHTML = durmin + ":"+dursec;
+});
+});
 
--- www/js/routes.js
+++ www/js/routes.js
@@ -11,6 +11,143 @@ routes = [
     path: '/page2/',
     url: './pages/page2.html',
   },
+      {
+    path: '/section1/',
+    url: './pages/section1.html',
+  },
+        {
+    path: '/section1a/',
+    url: './pages/section1a.html',
+  },
+ {
+    path: '/section1b/',
+    url: './pages/section1b.html',
+  },
+ {
+    path: '/section1c/',
+    url: './pages/section1c.html',
+  },
+       {
+    path: '/section2/',
+    url: './pages/section2.html',
+  },
+  {
+    path: '/section2a/',
+    url: './pages/section2a.html',
+  },
+  {
+    path: '/section2b/',
+    url: './pages/section2b.html',
+  },
+   {
+    path: '/section2c/',
+    url: './pages/section2c.html',
+  },
+   {
+    path: '/section2d/',
+    url: './pages/section2d.html',
+  },
+   {
+    path: '/section2e/',
+    url: './pages/section2e.html',
+  },
+ {
+    path: '/section3/',
+    url: './pages/section3.html',
+  },
+  {
+    path: '/section3a/',
+    url: './pages/section3a.html',
+  },
+  {
+    path: '/section3b/',
+    url: './pages/section3b.html',
+  },
+   {
+    path: '/section3c/',
+    url: './pages/section3c.html',
+  },
+   {
+    path: '/section3d/',
+    url: './pages/section3d.html',
+  },
+   {
+    path: '/section3e/',
+    url: './pages/section3e.html',
+  },
+   {
+    path: '/section3f/',
+    url: './pages/section3f.html',
+  },
+  {
+    path: '/section3g/',
+    url: './pages/section3g.html',
+  },
+        {
+    path: '/section4/',
+    url: './pages/section4.html',
+  },
+          {
+    path: '/section4a/',
+    url: './pages/section4a.html',
+  },
+          {
+    path: '/section4b/',
+    url: './pages/section4b.html',
+  },
+          {
+    path: '/section4c/',
+    url: './pages/section4c.html',
+  },
+          {
+    path: '/section4d/',
+    url: './pages/section4d.html',
+  },
+          {
+    path: '/section4e/',
+    url: './pages/section4e.html',
+  },
+          {
+    path: '/section5/',
+    url: './pages/section5.html',
+  },
+          {
+    path: '/section5a/',
+    url: './pages/section5a.html',
+  },
+           {
+    path: '/section5b',
+    url: './pages/section5b.html',
+  },
+
+           {
+    path: '/section6/',
+    url: './pages/section6.html',
+  },
+             {
+    path: '/section6a/',
+    url: './pages/section6a.html',
+  },
+             {
+    path: '/section6b/',
+    url: './pages/section6b.html',
+  },
+             {
+    path: '/section6c/',
+    url: './pages/section6c.html',
+  },
+             {
+    path: '/section6d/',
+    url: './pages/section6d.html',
+  },
+             {
+    path: '/section6e/',
+    url: './pages/section6e.html',
+  },
+            {
+    path: '/section7/',
+    url: './pages/section7.html',
+  },
   {
     path: '/catalog/',
     componentUrl: './pages/catalog.html',
@@ -34,6 +171,17 @@ routes = [
   {
     path: '/mediaPlayer/:id/',
     componentUrl: './pages/mediaPlayer.html'
+  },
+  {
+    path: '/mediaplayer/',
+    url: './pages/mediaplayer.html',
+  },
+  {
+    path: '/mhbsvideos/',
+	url: './pages/mhbsvideos.html',},
+   {
+    path: '/testvideo/',
+    url: './pages/testvideo.html',
   },
   // Page Loaders & Router
   {
